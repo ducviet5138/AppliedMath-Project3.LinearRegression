4fb1b7141f8a8c4ff72a72a0cc9d73bd26abbad4
ducviet5138 <ducviet5138@gmail.com>
2023-08-22 01:25:11 +0700

    Update model3

--- test.py
+++ test.py
@@ -1,7 +1,2 @@
-import numpy as np
-
-x = np.array([2, 2, 2])
-expo = np.array([1, 2, 3])
-mask = np.array([True, False, True])
-
-print(np.power(x[mask], expo[mask]))
+model3 = ["10percentage", "12percentage", "collegeGPA", "English", "Logical", "Quant", "conscientiousness", "agreeableness", "extraversion", "nueroticism", "openess_to_experience"]
+print(len(model3))
